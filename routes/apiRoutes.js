7741b9acdf11a7139e34eb8fd5e13641c7263644
Sean M Fitzpatrick <s3an.m4l.f1tz@gmail.com>
2023-02-21 15:31:50 -0500

    updated api routes to better route practice and now time to refractor the stoarge functionality

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,35 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const storage = require("../db/storage");
+
+// Router to delete notes from stoarge
+
+router.delete("/notes/:id", (req, res) => {
+  storage
+    .deleteNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+});
+
+// Router to GET notes from JSON DB and return as JSON
+
+router.get("./notes", (req, res) => {
+  storage
+    .getNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+// Router to POST a new note the user is wanting to save
+// First passed into the JSON DB and then returned to user in the sidebar
+
+router.post("./notes", (req, res) => {
+  storage
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
+
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,3 +1,17 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const path = require("path");
+
+// Router to serve the new note to the Notes.html file
+
+router.get("./notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+
+// Router to serve the new note to the Index.html file to be displayed to user
+
+router.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
 module.exports = router;
